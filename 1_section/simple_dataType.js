e134e33a0962e492f726d3a7c08cdf7bbebc6677
dogeonK <102578327+dogeonK@users.noreply.github.com>
2023-08-21 20:48:57 +0900

    [UPDATE] Operator

--- 1_section/simple_dataType.js
+++ 1_section/simple_dataType.js
@@ -6,3 +6,34 @@ console.log(num);
 
 let bool = true;
 console.log(bool);
+
+// 증가연산
+let a =1
+a++ // a=a+1과같다 
+console.log(a)  // 2
+
+//감소연산
+let b =2 
+b-- // b=b-1과 같다
+console.log(b)  // 1 
+
+//단축된 연산자
+let c =1
+c+=3 // c=c+3을 줄여서 표현한 것이다
+console.log(c)  //4
+
+let d = 3
+d-=2 // d=d-2를 줄여서 표현한 것이다 
+console.log(d)  //1
+
+// NOT 연산자 : 어떤값의 반대되는 값을 반환 true 면 false를, false면 true를 반환 
+let f = true
+console.log(!f) // false 
+
+// 같다라는 표현은 ==  
+let x = 2
+let y =2 
+console.log(x == y) // true
+
+//다르다라는 표현은 != 
+console.log(x != y) // false
