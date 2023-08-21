0e6529148174523ff12c150dda21f7b2a0e9d2cc
dogeonK <102578327+dogeonK@users.noreply.github.com>
2023-08-21 20:29:14 +0900

    [ADD] variable

---
+++ 1_section/variable.js
@@ -0,0 +1,14 @@
+let color = "black";
+color = "red";
+
+console.log(color);
+
+const color_const = "white";
+// color_const = "blue";
+
+console.log(color_const);
+
+var color_var = "pink";
+color_var = "yellow";
+
+console.log(color_var);
