d772213dfc94b060295fa8f9154d69a6584a9b75
dogeonK <102578327+dogeonK@users.noreply.github.com>
2023-08-24 21:11:57 +0900

    [UPDATE] switch, 삼항

--- 1_section/syntax.js
+++ 1_section/syntax.js
@@ -1,3 +1,4 @@
+// if
 let age = 18
 
 if(age > 20 && age < 30) {
@@ -15,3 +16,28 @@ let b = "1"
 
 console.log(a==b)
 console.log(a===b)
+
+// switch
+let menu = 3
+
+switch(menu) {
+    case 1 : {
+        console.log("물건 사기")
+        break
+    }
+    case 2 : {
+        console.log("잔고 확인")
+        break
+    }
+    case 3 : {
+        console.log("히스토리 확인")
+        break
+    }
+    default : {
+        console.log("홈으로 돌아가기")
+        break
+    }
+}
+
+// 삼항
+let answer = menu <=3 ? console.log("true") : console.log("false")
