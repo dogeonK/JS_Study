66f1cf9b10629943832c358108924b5e046684aa
dogeonK <102578327+dogeonK@users.noreply.github.com>
2023-08-24 20:44:57 +0900

    [ADD] if, else if, else

---
+++ 1_section/syntax.js
@@ -0,0 +1,17 @@
+let age = 18
+
+if(age > 20 && age < 30) {
+    console.log("if")
+}
+else if (age >= 18) {
+    console.log("else if")
+}
+else {
+    console.log("else")
+}
+
+let a = 1
+let b = "1"
+
+console.log(a==b)
+console.log(a===b)
