afde5ca653fb9fbb0b2a3aa57e388200f4e46824
dogeonK <102578327+dogeonK@users.noreply.github.com>
2023-08-25 20:24:22 +0900

    [ADD] function

---
+++ 1_section/fuc.js
@@ -0,0 +1,25 @@
+function makeBurger(type, size, num) {
+    console.log("빵")
+    console.log("상추")
+    if (type != null) {
+        console.log(type + "패티")
+    }
+    else {
+        console.log("고기패티")
+    }
+    console.log("빵")
+
+    console.log("사이즈 :" + size, num + "개")
+
+    return "준비 완료"
+}
+
+function serveCoke() {
+    console.log("얼음")
+    console.log("콜라")
+}
+
+console.log()
+let result = makeBurger("새우", "s", 2)
+
+console.log(result)
