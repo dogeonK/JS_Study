301b9842188e6c4e857314b99916e3ded7e4163c
dogeonK <102578327+dogeonK@users.noreply.github.com>
2023-08-28 18:44:21 +0900

    [UPDATE] 체크 버튼 동작 추가

--- second_project/main.js
+++ second_project/main.js
@@ -1,5 +1,4 @@
 // delete 버튼 클릭 -> 할 일 삭제
-// check 버튼 클릭 -> 할 일 종료 & 취소선 추가
 // All, Not Done, Done 클릭 -> under-line 이동
 // Done 탭에는 끝난 아이템만, Not Done 탭은 진행 중인 아이템만
 // All 탭은 전체 아이템
@@ -13,8 +12,13 @@ addButton.addEventListener("click", addTask);
 // 유저가 값 입력
 // + 버튼 클릭 -> 할 일 추가
 function addTask() {
-    let taskContent = taskInput.value;
-    taskList.push(taskContent);
+    let task = {
+        id: randomIDGenerate(),
+        taskContent: taskInput.value,
+        isComplete: false,
+    }
+
+    taskList.push(task);
     render();
 }
 // 할 일 추가 후 렌더링
@@ -22,14 +26,42 @@ function render() {
     let resultHTML = ''
 
     for (let i = 0; i < taskList.length; i++) {
-        resultHTML += `<div class="task">
-        <div>${taskList[i]}</div>
-        <div>
-            <button>Check</button>
-            <button>Delete</button>
-        </div>
-    </div>`;
+        if (taskList[i].isComplete) {
+            resultHTML += `<div class="task">
+            <div class="task-done">${taskList[i].taskContent}</div>
+            <div>
+                <button onclick="toggleComplete('${taskList[i].id}')">Check</button>
+                <button>Delete</button>
+            </div>
+        </div>`;
+        }
+        else {
+            resultHTML += `<div class="task">
+            <div>${taskList[i].taskContent}</div>
+            <div>
+                <button onclick="toggleComplete('${taskList[i].id}')">Check</button>
+                <button>Delete</button>
+            </div>
+        </div>`;
+        }
+
     }
 
     document.getElementById("task-board").innerHTML = resultHTML;
+}
+
+// check 버튼 클릭 (isComplete = true) -> 할 일 종료 & 취소선 추가
+function toggleComplete(id) {
+    for (let i = 0; i < taskList.length; i++) {
+        if (taskList[i].id == id) {
+            taskList[i].isComplete = !taskList[i].isComplete;
+            break;
+        }
+    }
+    render();
+}
+
+// task id 랜덤 생성
+function randomIDGenerate() {
+    return '_' + Math.random().toString(36).substr(2, 9);
 }
