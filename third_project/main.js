4201cdff2476bea6ab42a5d76540a916ab349471
dogeonK <102578327+dogeonK@users.noreply.github.com>
2023-09-07 02:03:17 +0900

    [UPDATE] news articles

--- third_project/main.js
+++ third_project/main.js
@@ -1,6 +1,8 @@
 import config from './apikey.js';
 const API_KEY = config.apiKey;
 
+let news = [];
+
 const getLatestNews = async()=>{
     let url = new URL(
         'https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&topic=tech'
@@ -10,7 +12,8 @@ const getLatestNews = async()=>{
     let response = await fetch(url, {headers: header});
     let data = await response.json();
 
-    console.log(data);
+    news = data.articles;
+    console.log(news);
 }
 
 getLatestNews();
