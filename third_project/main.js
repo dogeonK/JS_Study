d4a912f79363f63f4b3196692f1fa1ef9d3f7ea0
dogeonK <102578327+dogeonK@users.noreply.github.com>
2023-09-06 19:33:30 +0900

    [UPDATE] latest headlines

---
+++ third_project/main.js
@@ -0,0 +1,8 @@
+const getLatestNews = ()=>{
+    let url = new URL(
+        'https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&topic=tech'
+    );
+    let header = new Headers({'x-api-key': ''});
+}
+
+getLatestNews()
