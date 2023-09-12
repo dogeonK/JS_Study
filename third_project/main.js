7ea580fb6eeca35b2a64d99bf5e4a418141571f2
dogeonK <102578327+dogeonK@users.noreply.github.com>
2023-09-13 02:33:49 +0900

    [UPDATE] 페이지 렌더링 추가

--- third_project/main.js
+++ third_project/main.js
@@ -3,21 +3,6 @@ const API_KEY = apiKey;
 
 let news = [];
 
-const getLatestNews = async()=>{
-    let url = new URL(
-        'https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&topic=tech'
-    );
-    let header = new Headers({'x-api-key': `${API_KEY}`});
-
-    let response = await fetch(url, {headers: header});
-    let data = await response.json();
-
-    news = data.articles;
-    console.log(news);
-}
-
-getLatestNews();
-
 window.openNav = () => {
     document.getElementById("mySidenav").style.width = "250px";
 };
@@ -35,3 +20,43 @@ window.openSearchBox = () => {
         inputArea.style.display = 'inline';
     }
 }
+
+const getLatestNews = async()=>{
+    let url = new URL(
+        'https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&topic=tech'
+    );
+    let header = new Headers({'x-api-key': `${API_KEY}`});
+
+    let response = await fetch(url, {headers: header});
+    let data = await response.json();
+
+    news = data.articles;
+    console.log(news);
+
+    render();
+}
+
+const render = () => {
+    let newsHTML = ``;
+    
+    newsHTML = news.map(item => {
+        return `<div class="row news">
+        <div class="col-lg-4">
+            <img class="news-img-size" src="${item.media}" alt="">
+        </div>
+        <div class="col-lg-8">
+            <h2>${item.title}</h2>
+            <p>
+                ${item.summary}
+            </p>
+            <div>
+                ${item.rights} * ${item.published_date}
+            </div>
+        </div>
+    </div>`
+    })
+
+    document.getElementById("news-board").innerHTML = newsHTML.join("");
+}
+
+getLatestNews();
